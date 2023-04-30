5799aac86dbac197834368cdc7d3818e7d05fc06
Pedro Lopes <pedro_hlopes@outlook.com>
2023-04-30 11:31:00 -0300

    Create Ex1.c

---
+++ Programming fundamental/Class one/Ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Por mim se vai das dores a morada,\nPor mim se vai ao padecer eterno,\nPor mim se vai a gente condenada.\n");
+    printf("\nMoveu Justiça o Autor meu sempiterno,\nFormado fui por divinal possança, \nSabedoria suma e amor supremo.\n");
+    printf("\nNo existir, ser nenhum a mim se avanca,\nNao sendo eterno, e eu eternal perduro: \nDeixai, o vos que entrais toda a esperanca! \n");
+    printf("\nEstas palavras, em letreiro escuro, \nEu vi, por cima de uma porta escrito. \n'seu sentido' - disse eu - 'Mestre me é duro'\n");
+    
+    return 0;
+}
