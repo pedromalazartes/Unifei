e5c86aedd4b18588109a04dfd565399415e2d2fb
Pedro Lopes <pedro_hlopes@outlook.com>
2023-05-02 15:20:14 -0300

    Add files via upload

---
+++ Programming fundamental/Lesson Four/exemplo1.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+
+int main()
+{	
+	char letra;
+
+	printf("Digite um caractere: ");
+	scanf("%c", &letra);
+
+	printf("O caractere digitado foi: %c\n", letra);
+
+
+	return 0;
+}
